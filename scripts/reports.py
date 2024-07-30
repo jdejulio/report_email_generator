1b6a45fdc75ea894006339f6771df868ebfed8f2
julio <j_de_julio@hotmail.com>
2024-07-30 19:12:20 +0200

    Funciona el generador de reportes y tiene buen formato

--- scripts/reports.py
+++ scripts/reports.py
@@ -1,34 +1,5 @@
 #!/usr/bin/env python3
 
-
-import emails
-import os
-import reports
-
-table_data = [['Name', 'Amount', 'Value'],
-             ['elderberries', 10, 0.45],
-             ['figs', 5, 3],
-             ['apples', 4, 2.75],
-             ['durians', 1, 25],
-             ['bananas', 5, 1.99],
-             ['cherries', 23, 5.80],
-             ['grapes', 13, 2.48]]
-reports.generate("/tmp/report.pdf", "A Complete Inventory of My Fruit", "This is all my fruit.", table_data)
-
-sender = "sender@example.com"
-receiver = "{}@example.com".format(os.environ.get('USER'))
-subject = "List of Fruits"
-body = "Hi\n\nI'm sending an attachment with all my fruit."
-
-message = emails.generate(sender, receiver, subject, body, "/tmp/report.pdf")
-emails.send(message)
-
-student@7687732d6bd0:~/scripts$ cat            
-cars.py     emails.py   example.py  reports.py  
-student@7687732d6bd0:~/scripts$ cat reports.py 
-#!/usr/bin/env python3
-
-
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
